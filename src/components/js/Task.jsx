27465effbec2685797b8c43a1b31b2e990e48a25
Kaique Eduardo Lemes da Silva Cardoso <kaiquecardoso.kec@gmail.com>
2021-12-31 15:25:37 -0300

    mais retoque

--- src/components/js/Task.jsx
+++ src/components/js/Task.jsx
@@ -1,29 +1,33 @@
 import React from "react";
-import { useNavigate,  } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "../css/Task.css";
-import {CgClose, CgInfo} from 'react-icons/cg';
-const Task = ({task,handleTaskClick,handleTaskDelete}) => {
-let Navigate = useNavigate();
-const N = () =>{
-  Navigate(`/info`)
-}
+import { CgClose, CgInfo } from "react-icons/cg";
+const Task = ({ task, handleTaskClick, handleTaskDelete }) => {
+  let Navigate = useNavigate();
+  const N = () => {
+    Navigate(`/info`);
+  };
   return (
-
-<div className="task-container"
-style={task.completed ? {borderLeft: '6px solid chartreuse'}:{borderLeft: '6px solid transparent'}}>
- <div className="title"  onClick={() =>handleTaskClick(task.id)}>
- {task.title}
-   </div> <div>
-    <button className="info" onClick={N}>
-      <CgInfo/>
-    </button>
-   <button className="delete" onClick={() =>handleTaskDelete(task.id)}>
-     <CgClose/>
-   </button>
-
-   </div>
-  </div>
-
+    <div
+      className="task-container"
+      style={
+        task.completed
+          ? { borderLeft: "6px solid chartreuse" }
+          : { borderLeft: "6px solid transparent" }
+      }
+    >
+      <div className="title" onClick={() => handleTaskClick(task.id)}>
+        {task.title}
+      </div>{" "}
+      <div className="box">
+        <button className="info" onClick={N}>
+          <CgInfo />
+        </button>
+        <button className="delete" onClick={() => handleTaskDelete(task.id)}>
+          <CgClose />
+        </button>
+      </div>
+    </div>
   );
 };
 export default Task;
