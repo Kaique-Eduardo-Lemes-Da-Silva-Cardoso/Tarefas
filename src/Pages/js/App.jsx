b6111c962b3d02c412734526f72ea7751e7b2121
Kaique Eduardo Lemes da Silva Cardoso <kaiquecardoso.kec@gmail.com>
2022-02-13 12:41:10 -0300

    deleção pelo front no banco concluida

--- src/Pages/js/App.jsx
+++ src/Pages/js/App.jsx
@@ -4,71 +4,73 @@ import "../css/App.css";
 import AddTask from "../../components/js/AddTask";
 import Tasks from "../../components/js/Tasks";
 import axios from "axios";
+const baseURL = "http://localhost:3000";
 const App = () => {
-  const [tasks, setTasks] = useState([
-   
-   
-   
-  ]);
+  const [tasks, setTasks] = useState([]);
 
+  const fetchTasks = () => {
+    axios
+      .get("http://localhost:3000/GetData")
+      .then((response) => {
+        let data = tasks.concat(response.data);
+        console.log(data);
+        setTasks(data); /*commente essa linha para ver o show*/
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-  const fetchTasks =  () =>{
-    axios.get("http://localhost:3000/GetData")
-     .then(response => {
-       let data = tasks.concat(response.data);
-       console.log(data);
-     setTasks(data);/*commente essa linha para ver o show*/
-     })
-.catch(error =>{ console.log(error)})
-     }
+  const addTaskToDatabase = (taskTitle) => {
+    taskTitle = { title: taskTitle };
+    axios
+      .post("http://localhost:3000/CreateTask", taskTitle)
+      .then((response) => {
+        let data = tasks.concat(response.data);
+        console.log(data);
+        setTasks(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-
-const addTaskToDatabase = (taskTitle) =>{
-  taskTitle = {"title":taskTitle};
-axios.post("http://localhost:3000/CreateTask",taskTitle)
-.then(response =>{
-  let data = tasks.concat(response.data)
-  console.log(data)
-  setTasks(data);
-  
-
-
+const deleteOnDB = (taskId) =>{
+taskId = {"id":taskId}
+axios.post(`${baseURL}/DeleteOneTask`,taskId)
+.then((response)=>{
+  console.log(response.data)
 })
 .catch(error => {console.log(error)})
 }
 
 
-
   useEffect(() => {
-   
-      fetchTasks();
-
-    
+    fetchTasks();
   }, []);
 
-  const bagunça = () =>{
+  const bagunça = () => {
     tasks.map((task) => {
       console.log(`Task: id: ${task.id} titulo: ${task.title} 
       completed: ${task.completed}`);
     });
-  }
-  
-  const handleTaskAdd = (taskTitle) => {
-addTaskToDatabase(taskTitle);
-// const newtasks = [
-//   ...tasks,
-//   {
-//     title: taskTitle,
-//     id: data.id,
-//     completed: data.completed,
-//   },
-// ];
-// setTasks(newtasks);
+  };
 
-    
+  const handleTaskAdd = (taskTitle) => {
+    addTaskToDatabase(taskTitle);
+    // const newtasks = [
+    //   ...tasks,
+    //   {
+    //     title: taskTitle,
+    //     id: data.id,
+    //     completed: data.completed,
+    //   },
+    // ];
+    // setTasks(newtasks);
   };
 
   const handleTaskDelete = (taskId) => {
+    deleteOnDB(taskId);
     const newTasks = tasks.filter((task) => task.id !== taskId);
 
     setTasks(newTasks);
