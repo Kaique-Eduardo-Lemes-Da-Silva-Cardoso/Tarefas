d3ad3e235c7f2685c30c7f594912f92a2141891e
Kaique Eduardo Lemes da Silva Cardoso <kaiquecardoso.kec@gmail.com>
2022-02-16 14:51:39 -0300

    updates on info like new css and functions

--- src/Pages/js/Info.jsx
+++ src/Pages/js/Info.jsx
@@ -1,30 +1,50 @@
 import React from "react";
 import Button from "../../components/js/Button";
-import {useNavigate,useParams} from 'react-router-dom';
+import { useNavigate, useParams } from "react-router-dom";
 import "../css/Info.css";
-import { getByTitle } from "@testing-library/react";
+import axios from "axios";
+const baseURL = "http://localhost:3000";
+
 const Info = () => {
   const Navigate = useNavigate();
-  const {infoID,header} = useParams();
-  console.log(header)
-  console.log(infoID)
+  const { infoID, header } = useParams();
+  console.log(header);
+  console.log(infoID);
+
+  const updateInfo = (infoID, text) => {
+    //	"TaskId":"d3d31090-c64f-4a66-87d0-2f8a53c7d9b5",
+    // "text":"finally it works"
+    const update = { TaskID: infoID, text: text };
+
+    axios
+      .post(`${baseURL}/UpdateInfo`, update)
+      .then((Response) => {
+        console.log(Response.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <>
       <div className="container">
         <div className="c">
-          <h1>Info</h1>{`${header}`}
+          <h1>Info</h1>
+    
           <div className="btn">
-            <Button onClick={() =>{Navigate('/') }}>Voltar</Button>
+            <Button
+              onClick={() => {
+                Navigate("/");
+              }}
+            >
+              Voltar
+            </Button>
           </div>
         </div>
-        <p>{}</p>
-        <textarea className="texto"  >
-           💲💲💸💸💸💷💷  💲💲💸💸💸💷💷  💲💲💸💸💸💷💷  💲💲💸💸💸💷💷
-           💲💲💸💸💸💷💷  💲💲💸💸💸💷💷  💲💲💸💸💸💷💷  💲💲💸💸💸💷💷
-           💲💲💸💸💸💷💷  💲💲💸💸💸💷💷  💲💲💸💸💸💷💷  💲💲💸💸💸💷💷
-           💲💲💸💸💸💷💷  💲💲💸💸💸💷💷  💲💲💸💸💸💷💷  💲💲💸💸💸💷💷
-           💲💲💸💸💸💷💷  💲💲💸💸💸💷💷  💲💲💸💸💸💷💷  💲💲💸💸💸💷💷
-           💲💲💸💸💸💷💷  💲💲💸💸💸💷💷  💲💲💸💸💸💷💷  💲💲💸💸💸💷💷
+        <p>{`${header}`}</p>
+        <textarea className="texto" placeholder="Anote aqui...">
+
         </textarea>
       </div>
     </>
